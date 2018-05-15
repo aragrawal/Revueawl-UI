13c83f51f8ac8b2399752eb2e1329248318d8acb
Shriroop <shriroop.joshi@gmail.com>
2018-05-15 18:17:40 -0500

    Response received

--- src/js/App.js
+++ src/js/App.js
@@ -22,9 +22,28 @@ class App extends Component {
 		this.setState({city : event.target.value});
   }
 
-  handleSubmit () {
-        axios.get('http://localhost:3010/api/location/' + this.state.city)
-      .then(response => this.setSate({result : response.data}));
+  async handleSubmit () {
+    var location = 'http://localhost:3010/api/location/'
+    try {
+      console.debug("URL: " + location + this.state.city);
+      const resp = await axios.get(location + this.state.city)
+      console.log(resp.request.responseText);
+      // Since we are trying to use AJAX functionality, I think we should use async-await semantics
+      // I don't know why are we using axios library
+
+      // Ohh, and I don't like arrow syntax
+
+      // .then(function(resp) {
+      //   var respdata = resp.data;
+      //   console.log(respdata);
+      //   this.setState({result: respdata});
+      // })
+      // .catch(function(err) {
+      //   console.error(err);
+      // });
+    } catch(error) {
+      console.error(error);
+    }
   }
 
   render() {
@@ -38,7 +57,9 @@ class App extends Component {
           Your one stop for all the reviews
         </p>			 
 		<div class="container">
-  			<form class="form-inline">
+        {/* <form class="form-inline"> */}
+        {/* Using a form makes page reload and the changes are lost after reload. We can use a div, or some other container.
+        I don't want to mess up CSS-bootstrap, so leaving this as it is. */}
     			<div class="form-group">
       				<label for="city">City:</label>
       				<div class="col-4">
@@ -57,8 +78,8 @@ class App extends Component {
       					<input type="text" class="form-control" id="country" placeholder="Enter Country" name="country"></input>
       				</div>
     			</div>
-    				<button type="submit" class="btn btn-primary submitBtn" onClick={this.handleSubmit}>Submit</button>
-  			</form>
+    				<button class="btn btn-primary submitBtn" onClick={this.handleSubmit}>Submit</button>
+  			{/* </form> */}
 		</div>
       </div>
     );
